1c9ee8d0bbc0056813d7c5c71868e81b24b1a8d8
Rui Peres <sauron.is.alive@gmail.com>
2019-09-17 23:23:03 +0100

    Initial setup of state + routing in the MainViewModel

--- EightFourSevenTwo/Sources/Screens/MainViewModel.swift
+++ EightFourSevenTwo/Sources/Screens/MainViewModel.swift
@@ -1,6 +1,48 @@
 import SwiftUI
+import Combine
+import Core
+import StravaCore
 
 class MainViewModel: ObservableObject {
     
-    init() {}
+    @Published var state: State
+    let route: PassthroughSubject<Route, Never>
+    
+    private var disposables = Set<AnyCancellable>()
+    
+    init(session: SessionViewModel) {        
+        session
+            .state.map(toMainViewState)
+            .assign(to: \.state, on: self)
+            .store(in: &disposables)
+    }
+}
+
+extension MainViewModel {
+    enum State {
+        case loading
+        case authenticated(User)
+        case unauthenticated
+    }
+    
+    enum Route {
+        case presentStravaLogin(with: URLRequest)
+        case dismissStravaLogin
+        case showFailure(CoreError)
+    }
+}
+
+private func toMainViewState(_ state: SessionViewModel.State)
+    -> MainViewModel.State {
+        switch state {
+        case .authenticatingStep1,
+             .authenticatingStep2,
+             .checkCredentials,
+             .authenticated:
+            return .loading
+        case .unauthenticated,
+             .failedStep1,
+             .failedStep2:
+            return .unauthenticated
+        }
 }
